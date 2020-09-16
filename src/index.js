7a05f081f14ff35e8d8303a9ad6f193693d326a3
HarshithaVedagiri <harshigiri.hg@gmail.com>
2020-09-16 00:52:27 -0700

    my first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+
+class App extends React.Component{
+
+    state = {lat : null, errorMessage:'' };
+
+    componentDidMount()
+    {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState ({ lat: position.coords.latitude }),
+            err =>this.setState ({ errorMessage: err.message })
+        );
+    }
+
+    render(){
+
+            if (!this.state.lat && this.state.errorMessage)
+            return <div> Error: {this.state.errorMessage} </div>;
+
+            if (this.state.lat && !this.state.errorMessage)
+            return <SeasonDisplay lat={this.state.lat} />;
+
+            return <Spinner message="plesae accept the geoloc!ca"/>;
+        
+        
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
